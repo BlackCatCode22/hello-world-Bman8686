905ee4623188f524a99841acac7c5043a3f37ace
Bman8686 <110802592+Bman8686@users.noreply.github.com>
2024-01-21 19:04:39 -0800

    Add files via upload
    Change Value 3

---
+++ VariablesandMath5.java
@@ -0,0 +1,24 @@
+public class VariablesandMath5 {
+    public static void main(String[] args) {
+
+        System.out.println("\n Welcome to variables and math\n");
+
+        int a = 20;
+        double b = 5;
+        double sum = a + b;
+
+        System.out.println("\n\n The sum of " + a + " and " + b + " is " + sum);
+
+
+        double product = a * b;
+
+        System.out.println("The product of " + a + " and  " + b + " is " + product);
+
+        a = 75;
+        sum = a + b;
+        System.out.println("\n\n The new sum of " + a + " and  " + b + " is " + sum);
+
+
+    }
+
+}
