db1801e8dd1ba3ea3c2b666e3c9f4a8750aa98d5
Bman8686 <110802592+Bman8686@users.noreply.github.com>
2024-01-21 18:56:17 -0800

    Add files via upload
    Change Value 2

---
+++ VariablesandMath4.java
@@ -0,0 +1,22 @@
+public class VariablesandMath4 {
+    public static void main(String[] args) {
+
+        System.out.println("\n Welcome to variables and math\n");
+
+        int a = 10;
+        double b = 5.5;
+        double sum = a + b;
+
+        System.out.println("\n\n The sum of " + a + " and " + b + " is " + sum);
+
+
+        double product = a * b;
+
+        System.out.println("The product of " + a + " and  " + b + " is " + product);
+
+        a = 50;
+        sum = a + b;
+        System.out.println("\n\n The new sum of " + a + " and  " + b + " is " + sum);
+
+    }
+}
