e0e94164c549a33249b1376f6644e547ee4d3a5e
Bman8686 <110802592+Bman8686@users.noreply.github.com>
2024-01-21 17:24:36 -0800

    Add files via upload
    Sum

--- VariablesandMath.java
+++ VariablesandMath.java
@@ -3,12 +3,19 @@
 public class VariablesandMath {
     public static void main(String[] args) {
 
-        System.out.println("\n\nHello World!\n\n");
+        System.out.println("\n Welcome to variables and Math \n");
+
+        int a = 10;
+        double b = 5.5;
+        double sum = a + b;
+
+        System.out.println("\n\n The sum of " + a + " and " + b + " is " + sum);
+
 
     }
 
-}
 
+}
 
 
 
