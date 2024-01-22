bd6c828df50c03b35234a50936460f84b229579a
Bman8686 <110802592+Bman8686@users.noreply.github.com>
2024-01-21 18:12:06 -0800

    Add files via upload
    Product

---
+++ VariablesandMath2.java
@@ -0,0 +1,13 @@
+public class VariablesandMath2 {
+    public static void main(String[] args) {
+
+        System.out.println("\n Welcome to variables and math\n");
+
+    int a = 10;
+    double b = 5.5;
+    double product = a * b;
+
+    System.out.println("The product of " + a + " and  " + b + " is " + product);
+
+    }
+}
